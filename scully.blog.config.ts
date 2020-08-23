98a59e44ebc4659f4215aeec233fe21bd5304182
Americo Neto <americoneto1@gmail.com>
2020-08-23 02:30:58 -0300

    feat: adicionado scully

---
+++ scully.blog.config.ts
@@ -0,0 +1,16 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'blog',
+  pluginDir: './scully/plugins/',
+  outDir: './dist/static',
+  routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: './blog'
+      }
+    },
+  }
+};
